aec7c4792d97d2adacb583c664374a3f403d9067
Wessel Radstok <wessel.radstok@gmail.com>
2022-12-01 07:23:14 +0000

    Add day1 solution

---
+++ day01/solution.py
@@ -0,0 +1,22 @@
+import os
+
+with open(os.getcwd() +  "/day01/input.txt") as f:
+    lines = f.read().splitlines()
+
+# Store all elf snacks just in case we need them
+elf = []
+elf_weights = []
+for weight in lines:
+    if weight:
+        elf.append(int(weight))
+    else:  
+        elf_weights.append(elf)
+        elf = []
+elf_weights.append(elf)
+
+# Calculate number of calories each elf is carrying
+weights = list(map(sum, elf_weights))
+weights  = sorted(weights, reverse=True)
+print(f"Elf with most snacks has {weights[0]} calories")
+print(f"Top 3 elves with most snacks have {sum(weights[0:3])} calories")
+
