f94d24a2378520df31786bf0900f8e350ec67bf4
Wessel Radstok <wessel.radstok@gmail.com>
2022-12-07 20:43:11 +0000

    Cleanup day 7 using match syntax

--- day07/solution.py
+++ day07/solution.py
@@ -1,67 +1,68 @@
+# Turns out a lot of this code wasn't necessary, but you never know what part 2 brings :)
 import os
 from dataclasses import dataclass
 from typing import Optional
+
 
 @dataclass
 class File:
     name: str
     size: int
 
+
 @dataclass
 class Dir:
-    name: str
-    subdirs : dict[str, "Dir"]
+    subdirs: dict[str, "Dir"]
     files: list[File]
-    parent : Optional["Dir"]
+    parent: Optional["Dir"]
 
-    def get_all(self, at_most:int, found: int) -> int:
+    def sum_all_with_max_size(self, at_most: int, found: int) -> int:
         my_size = self.get_size()
         if my_size < at_most:
             found += my_size
-        
+
         for subdir in self.subdirs.values():
-            found = subdir.get_all(at_most, found)
+            found = subdir.sum_all_with_max_size(at_most, found)
         return found
 
-    def get_smallest(self, target: int, best: int) -> int:
+    def get_smallest_size(self, at_least: int, best: int) -> int:
         my_size = self.get_size()
-        if my_size > target and my_size < best:
+        if at_least < my_size < best:
             best = my_size
-        
+
         for subdir in self.subdirs.values():
-            best = subdir.get_smallest(target, best)
-        
+            best = subdir.get_smallest_size(at_least, best)
+
         return best
 
     def get_size(self) -> int:
-        return sum([file.size for file in self.files]) + sum([subdir.get_size() for subdir in self.subdirs.values()])
+        return sum((file.size for file in self.files)) + sum(
+            (subdir.get_size() for subdir in self.subdirs.values())
+        )
 
-with open(os.getcwd() +  "/day07/input.txt") as f:
+
+with open(os.getcwd() + "/day07/input.txt") as f:
     lines = f.read().splitlines()
 
-root = Dir("", dict(), [], None)
+root = Dir({}, [], None)
 curr_pos = root
 
-# Parse filestructure
 for line in lines:
-    if line.startswith("$"):
-        if line == "$ cd ..":
+    match line.split():
+        case "$", "cd", "..":
             curr_pos = curr_pos.parent
-        elif "cd /" in line:
+        case "$", "cd", "/":
             curr_pos = root
-        elif "cd" in line:
-            dir_name = line[5:]
-            curr_pos = curr_pos.subdirs[dir_name]
-    else:
-        if line.startswith("dir"):
-            name = line[4:]
-            curr_pos.subdirs[name] = Dir(name, dict(), [], curr_pos)
-        else:
-            size, name = line.split(" ")
+        case "$", "cd", name:
+            curr_pos = curr_pos.subdirs[name]
+        case "$", "ls":
+            continue
+        case "dir", name:
+            curr_pos.subdirs[name] = Dir({}, [], curr_pos)
+        case size, name:
             curr_pos.files.append(File(name, int(size)))
 
-print(f"Size of dirs on fs smaller than 100k: {root.get_all(100000, 0)}")
+print(f"Size of dirs on fs smaller than 100k: {root.sum_all_with_max_size(100000, 0)}")
 
-free = 70000000 - root.get_size()
-target = 30000000 - free
-print(f"Smallest to delete has size {root.get_smallest(target, 999999999999999999)}")
+at_least = 30000000 - (70000000 - root.get_size())
+print(f"Smallest to delete has size {root.get_smallest_size(at_least, 70000000)}")
